e9ffe0f69672f65843608f6d6f20b195fdc679ed
Dev1OptimusTecno <132096404+Dev1OptimusTecno@users.noreply.github.com>
2024-03-26 11:20:00 -0300

    ajuste na linha entre produto e preço

--- src/app/[id]/@produtos/_components/produto.tsx
+++ src/app/[id]/@produtos/_components/produto.tsx
@@ -7,10 +7,10 @@ export function ProdutoItem({ produto }: { produto: Produto }) {
             className={clsx("", produto.destaque == "1" && "text-destaque")}
             key={produto.cod_produto}
         >
-            <div className="flex flex-row items-center w-full">
-                <h3 className="text-lg flex-[3]">{produto.produto}</h3>
-                <span className="border-b-1 border-gray border-dotted flex-[2]"></span>
-                <div className="text-left flex-1 ml-4">{produto.valor}</div>
+            <div className="flex w-full items-center">
+                <h3 className="text-lg">{produto.produto}</h3>
+                <span className="flex-1 border-b-1 border-gray border-dotted"></span>
+                <div className="ml-4">{produto.valor}</div>
             </div>
             <p className="text-sm">{produto.descricao}</p>
         </div>
